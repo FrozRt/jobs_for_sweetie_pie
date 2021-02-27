3b56de9e2816adae8b139435520cb84768a1782c
Mikhail Chigrin <chmv@liis.su>
2021-02-27 23:41:58 +0300

    Add celery

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,11 +1,11 @@
 from aiogram import Dispatcher
 from loguru import logger
 
-from settings.config import admins
+from settings.config import settings
 from .middlewares import ThrottlingMiddleware, AccessMiddleware
 
 
 def setup(dp: Dispatcher):
     logger.info("Подключение middlewares...")
-    dp.middleware.setup(AccessMiddleware(admins))
+    dp.middleware.setup(AccessMiddleware(settings.admins))
     dp.middleware.setup(ThrottlingMiddleware())

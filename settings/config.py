d22a36b0e3a1eb1e0cd1bc958474bda91401ceb3
Mikhail Chigrin <chmv@liis.su>
2021-03-08 00:22:43 +0300

    Add db envs

--- settings/config.py
+++ settings/config.py
@@ -11,6 +11,16 @@ class Settings:
 
     admins = [os.getenv("ADMIN1"), os.getenv("ADMIN2")]
 
+    MY_DB = os.getenv("DB_NAME")
+    MY_HOST = os.getenv("DB_HOST")
+    MY_PORT = os.getenv("DB_PORT")
+    MY_USER = os.getenv("DB_USER")
+    MY_PASSWORD = os.getenv("DB_PASSWORD")
+
+    @classmethod
+    def get_db_connection_data(cls):
+        return {k[3:].lower(): v for k, v in cls.__dict__.items() if k[:3] == 'MY_'}
+
 
 settings = Settings()
-
+print(settings.get_db_connection_data())
